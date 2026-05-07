02ee890963d93c34f473b68533a6b19854aedc08
Peter <peterdogan1@gmail.com>
2026-05-06 21:15:22 -0700

    break things on purpose

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from dsc190-hw5!")
+import os
+import sys
 
+def main():
+    x=1+2
+    y = [1,2,3]
+    print("Hello from dsc190-hw5!")
 
 if __name__ == "__main__":
     main()
